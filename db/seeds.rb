26428834698b1fd02e467b3343783e7fdc7f427a
Brian Kennedy <briankennedy1@gmail.com>
2015-08-14 13:59:30 -0700

    Update migrations with all event fields. Start seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require 'csv'
+
+csv_text = File.read('~/Downloads/baseball-code/events/2014.csv')
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  Event.create!(row.to_hash)
+end
