8a21b41f80d85ba5fafb16817c25b54b0d80324a
Brian Kennedy <briankennedy1@gmail.com>
2015-08-17 10:33:24 -0700

    First draft of seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
 require 'csv'
 
-csv_text = File.read('events/2014.csv')
+csv_text = File.read('./events/2014.csv')
 csv = CSV.parse(csv_text, :headers => true)
 csv.each do |row|
   Event.create!(row.to_hash)
