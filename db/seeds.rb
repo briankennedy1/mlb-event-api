8a2a1960bd52d5d3f9c08684cf5d1d2afcd31f84
Brian Kennedy <briankennedy1@gmail.com>
2015-08-24 12:58:29 -0700

    Adjust seed file to injest multiple files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
 require 'csv'
 
-csv_text = File.read('./events/2014.csv')
-csv = CSV.parse(csv_text, :headers => true)
-csv.each do |row|
-  Event.create!(row.to_hash)
+files = Dir.glob('./events/*.csv')
+
+files.each do |csv_file|
+  csv_text = File.read(csv_file)
+  csv = CSV.parse(csv_text, :headers => true)
+  csv.each do |row|
+    Event.create!(row.to_hash)
+  end
+  p "*" * 50
+  p "Finished #{csv_file}"
+  p "*" * 50
 end
+#
+# csv_text = File.read('./events/2014.csv')
+# csv = CSV.parse(csv_text, :headers => true)
+# csv.each do |row|
+#   Event.create!(row.to_hash)
+# end
