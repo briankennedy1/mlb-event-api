a9e6d5493f732bc7f3368f3b166149c3cb3774b6
Brian Kennedy <briankennedy1@gmail.com>
2015-09-16 21:14:14 -0700

    Found a good strategy for seeding that shows progress, may still take forever but you know it's going

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,28 +2,33 @@ require 'csv'
 require 'ruby-progressbar'
 require 'smarter_csv'
 require 'parallel'
-require 'players'
+require_relative 'players'
 
-all_homers = Event.where(
-  bat_id: 'adamj901',
-  # event_cd: [20, 21, 22, 23]
-  event_cd: 23
-).order(:game_date, :id)
 
-pbar = ProgressBar.create(
-  starting_at: 0,
-  total: all_homers.count,
-  format: '%a %e %P% Processed: %c from %C'
-)
 
-all_homers.each do |current_event|
-  current_event.update_columns(
-  career_home_run: current_event.event_cd == 23 ?
-    all_homers.index(current_event) + 1 : nil,
-  season_home_run: current_event.event_cd == 23 ?
-    all_homers.by_year(current_event.game_date.year).index(current_event) + 1 : nil,
-  game_home_run: current_event.event_cd == 23 ?
-    all_homers.by_day(current_event.game_date).index(current_event) + 1 : nil
+PLAYERS.each do |player|
+
+  all_doubles = Event.where(
+    bat_id: player,
+    # event_cd: [20, 21, 22, 23]
+    event_cd: 21
+  ).order(:game_date, :id)
+
+  pbar = ProgressBar.create(
+    starting_at: 0,
+    total: all_doubles.length,
+    format: "Current player: #{player} %a %e %P% Processed: %c from %C"
   )
-  pbar.increment
+
+  all_doubles.each do |current_event|
+    current_event.update_columns(
+    career_double: current_event.event_cd == 22 ?
+      all_doubles.index(current_event) + 1 : nil,
+    season_double: current_event.event_cd == 22 ?
+      all_doubles.by_year(current_event.game_date.year).index(current_event) + 1 : nil,
+    game_double: current_event.event_cd == 22 ?
+      all_doubles.by_day(current_event.game_date).index(current_event) + 1 : nil
+    )
+    pbar.increment
+  end
 end
