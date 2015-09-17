0e3ec751871c25bfb91e15a34fe487e9434a0f10
Brian Kennedy <briankennedy1@gmail.com>
2015-09-17 10:08:22 -0700

    Create logic for singles

--- db/adding_doubles.rb
+++ db/adding_doubles.rb
@@ -2,7 +2,6 @@ require 'ruby-progressbar'
 require_relative 'players'
 
 PLAYERS.each do |player|
-
   all_doubles = Event.where(
     bat_id: player,
     # event_cd: [20, 21, 22, 23]
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,26 +2,26 @@ require 'ruby-progressbar'
 require_relative 'players'
 
 PLAYERS.each do |player|
-  all_doubles = Event.where(
+  all_singles = Event.where(
     bat_id: player,
     # event_cd: [20, 21, 22, 23]
-    event_cd: 21)
+    event_cd: 20)
     .order(:game_date, :id)
 
   pbar = ProgressBar.create(
     starting_at: 0,
-    total: all_doubles.length,
+    total: all_singles.length,
     format: "Current player: #{player} %a %e %P% Processed: %c from %C"
   )
 
-  all_doubles.each do |current_event|
+  all_singles.each do |current_event|
     current_event.update_columns(
-      career_double: all_doubles
+      career_single: all_singles
         .index(current_event) + 1,
-      season_double: all_doubles
+      season_single: all_singles
         .by_year(current_event.game_date.year)
         .index(current_event) + 1,
-      game_double: all_doubles
+      game_single: all_singles
         .by_day(current_event.game_date)
         .index(current_event) + 1
     )
