9497566614d13c948b693a3003cf56994dbdda26
Brian Kennedy <briankennedy1@gmail.com>
2015-09-18 10:23:54 -0700

    Change seeding to hits

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,26 +2,25 @@ require 'ruby-progressbar'
 require_relative 'players'
 
 PLAYERS.each do |player|
-  all_singles = Event.where(
+  all_hits = Event.where(
     bat_id: player,
-    # event_cd: [20, 21, 22, 23]
-    event_cd: 20)
+    event_cd: [20, 21, 22, 23])
     .order(:game_date, :id)
 
   pbar = ProgressBar.create(
     starting_at: 0,
-    total: all_singles.length,
+    total: all_hits.length,
     format: "Current player: #{player} %a %e %P% Processed: %c from %C"
   )
 
-  all_singles.each do |current_event|
+  all_hits.each do |current_event|
     current_event.update_columns(
-      career_single: all_singles
+      career_hit: all_hits
         .index(current_event) + 1,
-      season_single: all_singles
+      season_hit: all_hits
         .by_year(current_event.game_date.year)
         .index(current_event) + 1,
-      game_single: all_singles
+      game_hit: all_hits
         .by_day(current_event.game_date)
         .index(current_event) + 1
     )
