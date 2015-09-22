7acfac057f1c40a7d7d0268947ea7676e96fff4f
Brian Kennedy <briankennedy1@gmail.com>
2015-09-21 20:17:02 -0700

    Write seeds for pitching milestones

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,26 +2,31 @@ require 'ruby-progressbar'
 require_relative 'players'
 
 PLAYERS.each do |player|
-  all_hits = Event.where(
-    bat_id: player,
-    event_cd: [20, 21, 22, 23])
+  all_strikeouts = Event.where(
+    pit_id: player,
+    event_cd: 3)
     .order(:game_date, :id)
 
   pbar = ProgressBar.create(
     starting_at: 0,
-    total: all_hits.length,
+    total: all_strikeouts.length,
     format: "Current player: #{player} %a %e %P% Processed: %c from %C"
   )
 
-  all_hits.each do |current_event|
+  all_strikeouts.each do |current_event|
     current_event.update_columns(
-      career_hit: all_hits
+      pitcher_career_strikeout: all_strikeouts
         .index(current_event) + 1,
-      season_hit: all_hits
+      pitcher_season_strikeout: all_strikeouts
         .by_year(current_event.game_date.year)
         .index(current_event) + 1,
-      game_hit: all_hits
-        .by_day(current_event.game_date)
+      # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      # !! New code for doubleheaders
+      # !! Need to go back through batter events
+      # !! and fix
+      # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      pitcher_game_strikeout: all_strikeouts
+        .where(game_id: current_event.game_id)
         .index(current_event) + 1
     )
     pbar.increment
