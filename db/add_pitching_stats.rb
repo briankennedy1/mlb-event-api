28f57b9aa22a559a47b811483c89b95f43c9dc00
Brian Kennedy <briankennedy1@gmail.com>
2015-09-22 10:47:00 -0700

    Update logic for hits per game

---
+++ db/add_pitching_stats.rb
@@ -0,0 +1,29 @@
+require 'ruby-progressbar'
+require_relative 'players'
+
+PLAYERS.each do |player|
+  all_strikeouts = Event.where(
+    pit_id: player,
+    event_cd: 3)
+    .order(:game_date, :id)
+
+  pbar = ProgressBar.create(
+    starting_at: 0,
+    total: all_strikeouts.length,
+    format: "Current player: #{player} %a %e %P% Processed: %c from %C"
+  )
+
+  all_strikeouts.each do |current_event|
+    current_event.update_columns(
+      pitcher_career_strikeout: all_strikeouts
+        .index(current_event) + 1,
+      pitcher_season_strikeout: all_strikeouts
+        .by_year(current_event.game_date.year)
+        .index(current_event) + 1,
+      pitcher_game_strikeout: all_strikeouts
+        .where(game_id: current_event.game_id)
+        .index(current_event) + 1
+    )
+    pbar.increment
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,32 +2,22 @@ require 'ruby-progressbar'
 require_relative 'players'
 
 PLAYERS.each do |player|
-  all_strikeouts = Event.where(
-    pit_id: player,
-    event_cd: 3)
+  all_hits = Event.where(
+    bat_id: player,
+    event_cd: [20, 21, 22, 23])
     .order(:game_date, :id)
 
   pbar = ProgressBar.create(
     starting_at: 0,
-    total: all_strikeouts.length,
+    total: all_hits.length,
     format: "Current player: #{player} %a %e %P% Processed: %c from %C"
   )
 
-  all_strikeouts.each do |current_event|
+  all_hits.each do |current_event|
     current_event.update_columns(
-      pitcher_career_strikeout: all_strikeouts
-        .index(current_event) + 1,
-      pitcher_season_strikeout: all_strikeouts
-        .by_year(current_event.game_date.year)
-        .index(current_event) + 1,
-      # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-      # !! New code for doubleheaders
-      # !! Need to go back through batter events
-      # !! and fix
-      # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-      pitcher_game_strikeout: all_strikeouts
-        .where(game_id: current_event.game_id)
-        .index(current_event) + 1
+      game_hit: all_hits
+      .where(game_id: current_event.game_id)
+      .index(current_event) + 1
     )
     pbar.increment
   end
