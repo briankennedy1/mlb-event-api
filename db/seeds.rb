5dbf86082556fe463720a23cba4c68a9811991a7
Brian Kennedy <briankennedy1@gmail.com>
2015-09-22 19:12:34 -0700

    Seed game singles

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ require_relative 'players'
 PLAYERS.each do |player|
   all_hits = Event.where(
     bat_id: player,
-    event_cd: [20, 21, 22, 23])
+    event_cd: 20)
     .order(:game_date, :id)
 
   pbar = ProgressBar.create(
@@ -15,7 +15,7 @@ PLAYERS.each do |player|
 
   all_hits.each do |current_event|
     current_event.update_columns(
-      game_hit: all_hits
+      batter_game_single: all_hits
       .where(game_id: current_event.game_id)
       .index(current_event) + 1
     )
