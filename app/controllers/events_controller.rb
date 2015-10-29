7eda08b8ec8d15a3d21fea709997ee803925b554
Brian Kennedy <briankennedy1@gmail.com>
2015-10-29 11:58:40 -0700

    Caching is working on development

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ActionController::Base
   before_action :set_event, only: [:show, :update, :destroy]
 
-  caches_page :index
+  caches_page :index, :show_pitcher_events
   # caches_action :index, :show
 
   # Return the first 250 events if no event is specified.
