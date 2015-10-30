092fac5c3c0c269e04bb2101bcff94a490c31691
Brian Kennedy <briankennedy1@gmail.com>
2015-10-30 16:21:48 -0700

    Clean up formatting on games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,14 +4,17 @@ class GamesController < ApplicationController
   def all_games
     render json: {
       error: 'Request not specific enough',
-      message:
-        'Please use a specific game_id to look up games. API documentation: http://docs.mlbevents.apiary.io/'
+      message: 'Please use a specific game_id to look up games.
+      API documentation: http://docs.mlbevents.apiary.io/'
     }, status: 400
   end
 
   # Return all events from a specific game
   def show_game
-    @game = Event.where(game_id: params[:game_id]).order(:game_date, :id)
+    @game = Event.where(
+      game_id: params[:game_id]
+    ).order(:game_date, :id)
+
     render json: @game, status: 200
   end
 end
