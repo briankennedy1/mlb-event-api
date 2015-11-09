524b34abc97692eb28ebfa1f77ac399a1c553eab
Brian Kennedy <briankennedy1@gmail.com>
2015-11-09 15:01:41 -0800

    Indent and try to fix nil bug

--- db/add_batter_runs.rb
+++ db/add_batter_runs.rb
@@ -18,52 +18,56 @@ PLAYERS.each do |player|
   SEASON_COUNT = 0
   GAME_COUNT = 0
   # Set a marker on the groups that will be updated as we go through events
-  SEASON_MARKER = all_runs.first.game_date.year
-  GAME_MARKER = all_runs.first.game_id
+  SEASON_MARKER = all_runs.first.game_date.year || nil
+  GAME_MARKER = all_runs.first.game_id || nil
 
-  all_runs.each do |current_event|
-    if SEASON_MARKER != current_event.game_date.year
-      SEASON_MARKER = current_event.game_date.year
-      SEASON_COUNT = 0
+  if SEASON_MARKER.nil?
+    p 'No runs found'
+  else
+    all_runs.each do |current_event|
+      if SEASON_MARKER != current_event.game_date.year
+        SEASON_MARKER = current_event.game_date.year
+        SEASON_COUNT = 0
+      end
+
+      p "SEASON_MARKER: #{SEASON_MARKER}"
+      p "SEASON_COUNT: #{SEASON_COUNT}"
+
+      if GAME_MARKER != current_event.game_id
+        GAME_MARKER = current_event.game_id
+        GAME_COUNT = 0
+      end
+
+      p "GAME_MARKER: #{GAME_MARKER}"
+      p "GAME_COUNT: #{GAME_COUNT}"
+
+      # if player == current_event.bat_id
+      #   current_event.update_columns(
+      #     batter_career_run: CAREER_COUNT + 1,
+      #     batter_season_run: SEASON_COUNT + 1,
+      #     batter_game_run: GAME_COUNT + 1
+      #   )
+      # elsif player == current_event.base1_run_id
+      #   current_event.update_columns(
+      #     runner1_career_run: CAREER_COUNT + 1,
+      #     runner1_season_run: SEASON_COUNT + 1,
+      #     runner1_game_run: GAME_COUNT + 1
+      #   )
+      # elsif player == current_event.base2_run_id
+      #   current_event.update_columns(
+      #     runner2_career_run: CAREER_COUNT + 1,
+      #     runner2_season_run: SEASON_COUNT + 1,
+      #     runner2_game_run: GAME_COUNT + 1
+      #   )
+      # elsif player == current_event.base3_run_id
+      #   current_event.update_columns(
+      #     runner3_career_run: CAREER_COUNT + 1,
+      #     runner3_season_run: SEASON_COUNT + 1,
+      #     runner3_game_run: GAME_COUNT + 1
+      #   )
+      # end
+
+      pbar.increment
     end
-
-    p "SEASON_MARKER: #{SEASON_MARKER}"
-    p "SEASON_COUNT: #{SEASON_COUNT}"
-
-    if GAME_MARKER != current_event.game_id
-      GAME_MARKER = current_event.game_id
-      GAME_COUNT = 0
-    end
-
-    p "GAME_MARKER: #{GAME_MARKER}"
-    p "GAME_COUNT: #{GAME_COUNT}"
-
-    # if player == current_event.bat_id
-    #   current_event.update_columns(
-    #     batter_career_run: CAREER_COUNT + 1,
-    #     batter_season_run: SEASON_COUNT + 1,
-    #     batter_game_run: GAME_COUNT + 1
-    #   )
-    # elsif player == current_event.base1_run_id
-    #   current_event.update_columns(
-    #     runner1_career_run: CAREER_COUNT + 1,
-    #     runner1_season_run: SEASON_COUNT + 1,
-    #     runner1_game_run: GAME_COUNT + 1
-    #   )
-    # elsif player == current_event.base2_run_id
-    #   current_event.update_columns(
-    #     runner2_career_run: CAREER_COUNT + 1,
-    #     runner2_season_run: SEASON_COUNT + 1,
-    #     runner2_game_run: GAME_COUNT + 1
-    #   )
-    # elsif player == current_event.base3_run_id
-    #   current_event.update_columns(
-    #     runner3_career_run: CAREER_COUNT + 1,
-    #     runner3_season_run: SEASON_COUNT + 1,
-    #     runner3_game_run: GAME_COUNT + 1
-    #   )
-    # end
-
-    pbar.increment
   end
 end
